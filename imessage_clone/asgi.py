3061fd7aecda32be74241c4e5b6409ee0ae3dd98
Football-fanatic2024 <wilkiec904@gmail.com>
2026-01-05 13:43:38 -0500

    Update asgi.py

--- imessage_clone/asgi.py
+++ imessage_clone/asgi.py
@@ -1,18 +1,18 @@
 import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imessage_clone.settings")
 
 django_asgi_app = get_asgi_application()
 
+# ⭐ IMPORTANT: import routing *after* Django loads
+import chat.routing
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter(chat.routing.websocket_urlpatterns)
     ),
 })
